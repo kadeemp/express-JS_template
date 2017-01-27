97ecb402f9a054614a117bf3cb4d09d5e574669e
Kadeem Palacios <kadeempalacios@gmail.com>
2017-01-27 14:47:39 -0800

    challenges 1-2 complete

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
+//DECLARATIONS
 var express = require('express')
 var handlebars = require('express-handlebars');
 
 var app = express()
 
-
+//MIDDLEWARE
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//VARIABLES
+var names = [
+    {name:"Kevin"},
+    {name:"Kadeem"},
+    {name:"Kain"},
+    {name:"Ken"}
+]
 
+// RESPONSE INDEX
 app.get('/api/blahs',function(req, res) {
-    res.json
-} )
+    res.json({names:names});
+});
 // app.get('/', function (req, res) {
 //   res.render('layouts/home.handlebars')
 // })
 
-
+//PORT ACTIVATION
 app.listen(3000, function () {
   console.log('Server is Live!')
 })
